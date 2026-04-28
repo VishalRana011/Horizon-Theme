d0331eee1cd27c22e24750cd71e0caf3a70c0a8d
Vishal Rana <ranavishal001001@gmail.com>
2026-04-28 18:16:07 +0530

    Update custom.js

--- assets/custom.js
+++ assets/custom.js
@@ -1,3 +1,5 @@
 jQuery(document).ready(function(){
-  jQuery('.image-block').zoom({url: 'https://cdn.shopify.com/s/files/1/1029/3903/3681/files/xbox-image.png?v=1777374779'});
+  jQuery('.image-block').zoom({url: 'https://cdn.shopify.com/s/files/1/1029/3903/3681/files/xbox-image.png?v=1777374779', 
+    on: 'click'
+  });
 });
