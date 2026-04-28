7e836640babcf8bcadf5ef8f221d98817a427615
Vishal Rana <ranavishal001001@gmail.com>
2026-04-28 18:45:51 +0530

    Update custom.js

--- assets/custom.js
+++ assets/custom.js
@@ -1,4 +1,4 @@
 jQuery(document).ready(function(){
-  jQuery('.image-block').zoom({url: 'cdn/shop/files/gaming-console-image.png', on: 'toggle',touch: false
+  jQuery('.image-block').zoom({url: 'cdn/shop/files/gaming-console-image.png', on: 'click',touch: true
   });
 });
