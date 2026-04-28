9fccae6dcec3b318c297091ae5f77ffe9360cf91
Vishal Rana <ranavishal001001@gmail.com>
2026-04-28 17:04:57 +0530

    modified

--- assets/custom.js
+++ assets/custom.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-$('.image-block img').zoom({url: 'xbox-image.png'});
+  if ($.fn.zoom) {
+    $('.image-block img').zoom({
+      url: 'xbox-image.png'
+    });
+  } else {
+    console.log("Zoom plugin not loaded");
+  }
 });
